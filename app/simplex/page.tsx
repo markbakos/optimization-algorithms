843d47b0d9c8c290b32190d3fec356bfcd6b8e3c
markbakosss <markbakosss@gmail.com>
2026-01-13 19:12:35 +0100

    add page for displaying workbench

---
+++ app/simplex/page.tsx
@@ -0,0 +1,9 @@
+import { Workbench } from "@/components/simplex/Workbench";
+
+export default function SimplexPage() {
+    return (
+        <main className="min-h-screen bg-neutral-100">
+            <Workbench />
+        </main>
+    );
+}
